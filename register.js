c66287a6375328d27cbd010fc83d48deefe78a2b
Ahmed Hamed <99084077+ahmed-hamed0@users.noreply.github.com>
2025-03-08 12:00:31 +0200

    Update register.js

--- register.js
+++ register.js
@@ -1,5 +1,6 @@
  // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+ import { getAuth , createUserWithEmailAndPassword } from "firebase/auth";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
