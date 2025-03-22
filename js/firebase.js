8a415248079c58d7d9ffb8ec89d1341105eb2494
Ahmed Hamed <99084077+ahmed-hamed0@users.noreply.github.com>
2025-03-22 20:25:36 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,22 +1,20 @@
 // تهيئة Firebase
-  firebase.initializeApp(firebaseConfig);
-  window.auth = firebase.auth();
-  window.db = firebase.firestore();
-}
-
-
-// تهيئة Firebase
 const firebaseConfig = {
-   apiKey: "AIzaSyDxd_9W5-Qc8rqSfGrKogla3xmHBX8liIg",
-    authDomain: "sani3ydotcom.firebaseapp.com",
-    projectId: "sani3ydotcom",
-    storageBucket: "sani3ydotcom.appspot.com",
-    messagingSenderId: "880517005136",
-    appId: "1:880517005136:web:e7f08efdadee45ec943655"
+  apiKey: "AIzaSyDxd_9W5-Qc8rqSfGrKogla3xmHBX8liIg",
+  authDomain: "sani3ydotcom.firebaseapp.com",
+  projectId: "sani3ydotcom",
+  storageBucket: "sani3ydotcom.appspot.com",
+  messagingSenderId: "880517005136",
+  appId: "1:880517005136:web:e7f08efdadee45ec943655"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Initialize Firestore
+// Initialize Firebase Authentication and Firestore
+const auth = firebase.auth();
 const db = firebase.firestore();
+
+// جعل المتغيرات متاحة بشكل عام
+window.auth = auth;
+window.db = db;
