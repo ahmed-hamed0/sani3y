a1f9dbaca372ae3867d0f9f59c3d155940ecd819
Ahmed Hamed <99084077+ahmed-hamed0@users.noreply.github.com>
2025-03-25 13:08:17 +0200

    Create login.js

---
+++ login.js
@@ -0,0 +1,18 @@
+// login.js
+import { auth } from './firebase.js';
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      alert('تم تسجيل الدخول بنجاح!');
+      window.location.href = 'profile.html';
+    })
+    .catch((error) => {
+      alert('خطأ: ' + error.message);
+    });
+});
