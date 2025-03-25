28b398f3bbd544f5ab12ca6ab375d407bb5f2cba
Ahmed Hamed <99084077+ahmed-hamed0@users.noreply.github.com>
2025-03-25 14:48:07 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxd_9W5-Qc8rqSfGrKogla3xmHBX8liIg",
+  authDomain: "sani3ydotcom.firebaseapp.com",
+  projectId: "sani3ydotcom",
+  storageBucket: "sani3ydotcom.appspot.com",
+  messagingSenderId: "880517005136",
+  appId: "1:880517005136:web:e7f08efdadee45ec943655"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
