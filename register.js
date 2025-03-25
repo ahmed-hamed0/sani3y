b44d22a112b303a8d1298d1766271cc8eb1b5fe1
Ahmed Hamed <99084077+ahmed-hamed0@users.noreply.github.com>
2025-03-25 14:16:35 +0200

    Update register.js

--- register.js
+++ register.js
@@ -1,23 +1,30 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-import { getAuth, createUserWithEmailAndPassword } from "https://www.getatic.com/firebasejs/10.7.2/firebase-auth.js";
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { 
+  getFirestore, 
+  doc, 
+  setDoc 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDxd_9W5-Qc8rqSfGrKogla3xmHBX8liIg",
-    authDomain: "sani3ydotcom.firebaseapp.com",
-    projectId: "sani3ydotcom",
-    storageBucket: "sani3ydotcom.firebasestorage.app",
-    messagingSenderId: "880517005136",
-    appId: "1:880517005136:web:e7f08efdadee45ec943655"
+  apiKey: "AIzaSyDxd_9W5-Qc8rqSfGrKogla3xmHBX8liIg",
+  authDomain: "sani3ydotcom.firebaseapp.com",
+  projectId: "sani3ydotcom",
+  storageBucket: "sani3ydotcom.appspot.com", // تم تصحيح هذا الحقل
+  messagingSenderId: "880517005136",
+  appId: "1:880517005136:web:e7f08efdadee45ec943655"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
 
 // DOM Elements
 const signupForm = document.getElementById('signupForm');
