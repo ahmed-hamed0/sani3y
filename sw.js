45682a7a2f50551bce3f2b43c27557ebb682cab0
Ahmed Hamed <99084077+ahmed-hamed0@users.noreply.github.com>
2025-04-07 11:34:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,24 @@
 // sw.js
-self.addEventListener('install', (e) => {
-  console.log('Service Worker installed');
+const CACHE_NAME = 'sani3y-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/assets/image/toolsman.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
